9cf8bc8df41a566c6faba376e335f13b58736502
AyaRagab555 <eng.aya.ragab@gmail.com>
2023-06-08 02:50:45 +0300

    Aya-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Typography } from '@mui/material';
 import './App.css';
-import {BrowserRouter as Router , Route , Routes } from "react-router-dom"
-import Home from "../src/componants/Home/Home"
-import About from "../src/componants/About/About"
-import Contact from "../src/componants/Contact/Contact"
-import Header from "../src/componants/Header/Header"
-import Footer from "../src/componants/Footer/Footer"
+import {BrowserRouter as Router , Route , Routes } from "react-router-dom";
+import Home from "../src/componants/Home/Home";
+import About from "../src/componants/About/About";
+import Contact from "../src/componants/Contact/Contact";
+import Header from "../src/componants/Header/Header";
+import Footer from "../src/componants/Footer/Footer";
 
 
 function App() {
