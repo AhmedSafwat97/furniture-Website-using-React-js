b62988d4caffe5cce914aeef0dfbfbd3072ed968
AhmedSafwat97 <xppahmad@gmail.com>
2023-06-16 05:56:23 +0300

    adding redux tool kit

---
+++ Api/products.js
@@ -0,0 +1,51 @@
+const products = [
+ {
+   id: 1,
+   Name: "Sofa",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 100,
+   imageLink: "../../Imgs/Products/sofa.png",
+ },
+ {
+   id: 2,
+   Name: "Chair",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 200,
+   imageLink: "../../Imgs/Products/chair.png",
+ },
+ {
+   id: 3,
+   Name: "Komod",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 300,
+   imageLink: "../../Imgs/Products/komod.png",
+ },
+ {
+   id: 4,
+   Name: "Dresser",
+   description:
+     "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+   price: 400,
+   imageLink: "../../Imgs/Products/dresser.png",
+ },
+ {
+  id: 5,
+  Name: "Table",
+  description:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+  price: 400,
+  imageLink: "../../Imgs/Products/table.png",
+},
+{
+  id: 6,
+  Name: "Bed",
+  description:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+  price: 400,
+  imageLink: "../../Imgs/Products/Bed room.png",
+},
+];
+module.exports = products;
