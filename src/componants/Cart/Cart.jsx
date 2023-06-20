26e34852a1fdb4a33de076430ab34f2133e7a176
AyaRagab555 <eng.aya.ragab@gmail.com>
2023-06-20 03:01:27 +0300

    newCard

--- src/componants/Cart/Cart.jsx
+++ src/componants/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ScrollToTop from "../../ExternalMethods/ScrollToTop";
 import BannerSection from "../Home/HomeSections/bannerSection";
-import { Box, Button, IconButton, Rating, Stack, Typography } from '@mui/material';
+import { Box, Button, IconButton, Input, Rating, Stack, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
@@ -15,8 +15,8 @@ const Cart = () => {
     <BannerSection  PageName="Shopping Cart" />
 
 
-    <Box sx={{width : "100%" , minHeight :"100vh" , display : "flex" , justifyContent : "center"}}>
-        <Box className="container" sx={{ my : "50px" ,width : "75%" , height : "fit-content" , border : "0.5px solid #E9E7DB"}}>
+    <Box sx={{width : "100%" , minHeight :"50vh" , display : "flex" , justifyContent : "center"}}>
+        <Box className="container" sx={{ my : "50px" ,width : {xs : "95%", md : "75%"} , height : "fit-content" , border : "0.5px solid #E9E7DB"}}>
 
 
 
@@ -96,6 +96,84 @@ const Cart = () => {
 
 
        </Box>
+
+
+
+    </Box>
+
+    <Box sx={{width : "100%" , display : "flex" , justifyContent : "center"}}>
+        <Box sx={{width :"75%", display:"flex",justifyContent:"space-between",flexWrap : "wrap" }}>   
+            <Box sx={{width:{xs:"90%" ,md:"47%"},height:"330px",my:"30px" , border:"1px solid gray", borderRadius:"15px"}}>
+                <Box sx={{display: "flex",flexDirection:"column", alignItems:"start" , justifyContent: "center", m:"20px"}}>
+                    <Typography sx={{fontWeight:"bold",m:"10px", mx:"auto"}}>Coupon Code</Typography>
+                    <Box sx={{width: "100%", height:"1px",bgcolor:"#E9E7DB"}} />
+                    <Typography sx={{fontSize:"12px",color:"gray",m:"20px"}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique iste ,Lorem ipsum dolor sit amet consectetur adipisicing elit. In, magni! Quaerat, ad. Nesciunt, quidem earum. Voluptatibus illo enim, eveniet, repellat ad ducimus quod quae ratione.</Typography>
+                    <input placeholder="coupon Code" style={{width:"90%",backgroundColor:"#E9E7DB",margin:"10px 20px",border: "none",borderRadius:"15px",padding:"10px"}} ></input>
+                    <Button
+                        sx={{
+                        fontSize: "12px",
+                        mx: "20px",
+                        backgroundColor: "#AC8C5B",
+                        fontWeight: "200",
+                        cursor: "pointer",
+                        color: "#FFF",
+                        padding: "5px 20px",
+                        borderRadius: "20px",
+                        ":hover": { color: "#AC8C5B", outline: "1px solid #AC8C5B" },
+                        }}
+                    >
+                        Apply Coupon
+                    </Button>
+                
+                </Box>
+            </Box>
+            <Box sx={{width:{xs:"90%" ,md:"47%"},height:"330px",my:"30px" , border:"1px solid gray", borderRadius:"15px"}}>
+
+                <Box sx={{width:"90%" ,display: "flex",flexDirection:"column", alignItems:"start" , justifyContent: "center", m:"20px"}}>
+                    <Typography sx={{fontWeight:"bold",m:"10px", mx:"auto"}}>Cart Totals</Typography>
+                    <Box sx={{width: "100%", height:"1px",bgcolor:"#E9E7DB"}} />
+                    <Box sx={{width:"100%",display:"flex", justifyContent:"space-between"}}>
+                        <Typography sx={{m:"10px"}}>SubTotal</Typography>
+                        <Typography sx={{color:"gray",m:"10px"}}>$3,540</Typography>
+                    </Box>
+                    <Box sx={{width: "100%", height:"1px",bgcolor:"#E9E7DB"}} />
+                    <Box sx={{width:"100%",display:"flex", justifyContent:"space-between"}}>
+                        <Typography sx={{m:"3px 10px"}}>Shipping</Typography>
+                        <Typography sx={{color:"gray",m:"3px 10px"}}>Free Shipping</Typography>
+                    </Box>
+                    <Box sx={{width:"100%",display:"flex", justifyContent:"space-between"}}>
+                        <Typography sx={{m:"3px 10px"}}>Tax</Typography>
+                        <Typography sx={{color:"gray",m:"3px 10px"}}>$00</Typography>
+                    </Box>
+                    <Box sx={{width:"100%",display:"flex", justifyContent:"space-between"}}>
+                        <Typography sx={{m:"3px 10px"}}>Local Delivery</Typography>
+                        <Typography sx={{color:"gray",m:"3px 10px"}}>$20</Typography>
+                    </Box>
+                    <Box sx={{width: "100%", height:"1px",bgcolor:"#E9E7DB"}} />
+                    <Box sx={{width:"100%",display:"flex", justifyContent:"space-between"}}>
+                        <Typography sx={{m:"10px"}}>Total</Typography>
+                        <Typography sx={{color:"gray",m:"10px"}}>$3,560</Typography>
+                    </Box>
+                    <Button
+                        sx={{
+                        fontSize: "12px",
+                        mx: "10px",
+                        backgroundColor: "#AC8C5B",
+                        fontWeight: "200",
+                        cursor: "pointer",
+                        color: "#FFF",
+                        padding: "5px 20px",
+                        borderRadius: "20px",
+                        ":hover": { color: "#AC8C5B", outline: "1px solid #AC8C5B" },
+                        }}
+                    >
+                        CheckOut
+                    </Button>
+                
+                </Box>
+            </Box>
+
+        </Box>
     </Box>
 
 
