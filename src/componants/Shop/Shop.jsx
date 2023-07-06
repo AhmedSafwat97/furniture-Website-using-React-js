05fdb2948410ef679284bc6d3904ccdfde0f7fe4
AhmedSafwat97 <xppahmad@gmail.com>
2023-07-06 08:13:05 +0300

    ......

--- src/componants/Shop/Shop.jsx
+++ src/componants/Shop/Shop.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     Box,
     Button,
@@ -6,7 +7,6 @@ import {
     IconButton,
     Rating,
     Stack,
-    Tab,
     TextField,
     Typography,
     useMediaQuery,
@@ -16,7 +16,7 @@ import {
     FormGroup
   } from
     "@mui/material";
-  import React, { useState } from "react";
+  import { useState } from "react";
   import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
   import FavoriteIcon from "@mui/icons-material/Favorite";
   import SearchIcon from "@mui/icons-material/Search";
@@ -32,275 +32,283 @@ import ScrollToTop from "../../ExternalMethods/ScrollToTop";
 import BannerSection from "../Home/HomeSections/bannerSection";
 
 const Shop = () => {
-    const { data, error, isLoading } = useGetproductByNameQuery();
-    const { SelectedProductsId , SelectedProducts  } = useSelector((state) => state.Cart);
-    const { favProductsId   } = useSelector((state) => state.Fav);
-    const [ checked , SetChecked ] =  useState("");
-    const dispatch = useDispatch();
-    const Navigate = useNavigate();
-   
-    const uniqueProducts = [];
-    const categories = [];
+
+  const Tags = [
+    {Tag : "Chairs"} , 
+    {Tag : "BedRoom"} , 
+    {Tag : "Bed"} , 
+    {Tag : "Cabinet"} , 
+    {Tag : "Sofa"} , 
+    {Tag : "Decor"} , 
+    {Tag : "Office"} , 
+    {Tag : "Lamp"} , 
+    {Tag : "Accent Furniture"} , 
+    {Tag : "Special Offer"} , 
+  ]
 
 
-    if (data) {
-    // Iterate over each product
-    for (const product of data) {
-        const category = product.category;
-    
-        // Check if the category is already added to the uniqueProducts array
-        if (!categories.includes(category)) {
-        uniqueProducts.push(product);
-        categories.push(category);
-        }
-    }
-    
-    }
+  const { data, error, isLoading } = useGetproductByNameQuery();
+  const { SelectedProductsId , SelectedProducts  } = useSelector((state) => state.Cart);
+  const { favProductsId   } = useSelector((state) => state.Fav);
+  const [ checked , SetChecked ] =  useState(["All"]);
+  const dispatch = useDispatch();
+  const Navigate = useNavigate();
+ 
 
-    const valueFilter = (checked)=>{
-        return checked === ""? data : data.filter((product)=> product.category === checked);
-    }
 
-    function ProductQuantity(item) {
-        const useritem = SelectedProducts.find((userselect) => {
-          return userselect.id === item.id;
-        });
-        return useritem.quantity;
+
+//   ________________________________________________________________
+  const uniqueProducts = [];
+  const categories = [];
+
+  if (data) {
+    // ____________________________________
+    // this code is used to get only one product for each category 
+    // ____________________________________
+  // Iterate over each product
+  for (const product of data) {
+      const category = product.category;
+      // Check if the category is already added to the uniqueProducts array
+      if (!categories.includes(category)) {
+      uniqueProducts.push(product);
+      categories.push(category);
       }
-  
-      const [value, setValue] = React.useState(0);
-  
-      const handleChange = (event, newValue) => {
-        setValue(newValue);
-      };
-      const theme = useTheme();
-      const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
-    return (
-        <Box>
-            <ScrollToTop/>
-            <BannerSection PageName="Shop"/>
-            <Box
-            sx={{
-                minWidth: "75%"  ,
-                maxWidth:"75%",
-                mx:"auto",
-            bgcolor: "#FFF",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            justifyContent: "center",
-            my:"50px"}}>
+  }
+  }
+//_________________________________________________________________
 
-            {error && 
+
+
+  const valueFilter = (checked)=>{
+      return checked.includes("All")? data : data.filter((product)=> product.category === checked);
+  }
+
+  function ProductQuantity(item) {
+      const useritem = SelectedProducts.find((userselect) => {
+        return userselect.id === item.id;
+      });
+      return useritem.quantity;
+    }
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+    
+
+
+    return (
+        <>
+    <ScrollToTop/>
+    <BannerSection  PageName="Shop" />
+
+    {error && 
    <Box  sx={{display : "flex" , justifyContent : "center" , alignItems : "center" ,width : "100%" , height : "100%"}}>
     <Typography variant="h6">Data Not Found</Typography>
-    </Box>}
+    </Box>
+   }
 
 
      {isLoading && 
       <Box sx={{display : "flex" , justifyContent : "center" , alignItems : "center" ,width : "100%" , height : "100%"}}>
-       <CircularProgress />
-      </Box>}
+      <CircularProgress />
+  </Box>
+     
+     }
 
+<Box sx={{ p : "50px 0" , width : "100%" , display : "flex" , justifyContent : "center" , border : "2px solid black"}}>
 
-            {data &&
-     <Box sx={{ display : "flex" , 
-     justifyContent : "center"}}>
-        <Box
-        sx={{
-            minWidth:"75%" ,
-            display : "flex" , 
-            justifyContent : "start" , 
-            flexWrap :"wrap" ,
-        }}>
-            {valueFilter(checked).map((Product)=>
-        
-        (
-        <Box
-        key={Product.id}
-        className="Card"
-        sx={{
-            width : {xs:"180px" , md: "240px"} ,
-            height: "290px",
-            backgroundColor: "#F3F2EE",
-            borderRadius: "15px",
-            padding: "10px",
-            position: "relative",
-            overflow: "hidden",
-            cursor: "pointer",
-            m : "10px"
-        }}
+{data && 
 
-        >
-        {Product.discount &&
-        <Chip
-        label={Math.floor( 100 -  (Product.sale / Product.price) * 100) +"%"}
-        sx={{
-            height: "20px",
-            borderRadius: "5px",
-            backgroundColor: "#AC8C5B",
-            color: "#FFF",
-            position: "absolute",
-        }}
-        />
-
-        }
-    {favProductsId.includes(Product.id) ?
-    (
-    <IconButton
-        onClick={() => {
-        dispatch(deleteFromFav(Product))
-        }}
-        sx={{ color: "gray", position: "absolute", right: "5px" }}
-    >
-        <FavoriteIcon />
-    </IconButton>   
-    ) : (
-    <IconButton
-            onClick={() => {
-        dispatch(AddToFav(Product))
-                }}
-                sx={{ color: "gray", position: "absolute", right: "5px" }}
-                >
-                <FavoriteBorderIcon />
-                </IconButton>   
-    )
+<>
+    <Box sx={{display : "flex" , flexWrap : "wrap" ,justifyContent : "center" , width : {xs : "100%" , md : "58%" }}}>
     
-    
-    
-    }
-    
-        <Box sx={{ width: "75%", height: "65%", mx: "auto" }}  
-            onClick={() => {
-            Navigate(`/prodetails/${Product.id}`)
-            }}
-
-        >
-            <img
-            style={{ width: "100%", height: "100%" }}
-            src={Product.imageLink}
-            alt=""
-            />
-        </Box>
-        <Box
-            className="cardcontent"
+    {valueFilter(checked).map((Product)=>
+            
+            (
+            <Box
+            key={Product.id}
+            className="Card"
             sx={{
-            p: "5px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            bgcolor: "#FFF",
-            mx: "auto",
-            width: "95%",
-            height: "32%",
-            borderRadius: "15px",
+                width : {xs:"180px" , md: "210px"} ,
+                height: "290px",
+                backgroundColor: "#F3F2EE",
+                borderRadius: "15px",
+                padding: "10px",
+                position: "relative",
+                overflow: "hidden",
+                cursor: "pointer",
+                m : "10px"
             }}
-        >
-            <Typography variant="h6">{Product.Name}</Typography>
-            <Stack spacing={1}>
-            <Rating
-                name="half-rating-read"
-                defaultValue={Product.rate}
-                precision={0.5}
-                readOnly
+    
+            >
+            {Product.discount &&
+            <Chip
+            label={Math.floor( 100 -  (Product.sale / Product.price) * 100) +"%"}
+            sx={{
+                height: "20px",
+                borderRadius: "5px",
+                backgroundColor: "#AC8C5B",
+                color: "#FFF",
+                position: "absolute",
+            }}
             />
-            </Stack>
-            <Box sx={{ mt: "5px", display: "flex" }}>
-            <del style={{ marginRight: "7px" }}> ${Product.price}</del>
-            <Typography>${Product.sale}</Typography>
-            </Box>
-
-
-
-            {SelectedProductsId.includes(Product.id) ?
-
-        (<Box className="btn-tocart" display="none">
-        <Box  sx={{ height : "27%", display : "flex"  , alignItems : "center" , justifyContent : "center"}}>
-        <Box sx={{display : "flex" , alignItems : "center" , width : "75%" , justifyContent : "center"}}>
-            <IconButton 
-            
-            onClick={() => {
-                dispatch(decrement(Product))
-            }}
-            sx={{bgcolor : "#FFF", 
-            ":hover" : {
-                bgcolor : "#ac8c5b"
-                }
-            
-            }} size="small" >
-                <RemoveIcon sx={{color : "#ac8c5b" , ":hover" : {
-                color : "#FFF"
-                }}} />
-            </IconButton>
-            <Typography sx={{mx:"5px" , fontWeight : "bold"}}>{ProductQuantity(Product)}</Typography>
-            <IconButton
-            onClick={() => {
-                dispatch(increment(Product))
-            }}
-            
-            sx={{bgcolor : "#FFF" , ":hover" : {
-                bgcolor : "#ac8c5b"
-                }}} size="small">
-                <AddIcon sx={{color : "#ac8c5b" , ":hover" : {
-                color : "#FFF"
-                }}} />
-            </IconButton>
-        </Box>
-
-        </Box>
-        </Box>
-
-        ) : 
-
-
-        (
-        <Button
-        className="btn-tocart"
-        sx={{ bgcolor: "#AC8C5B", display: "none" ,
-        ":hover": {
-        color: "#ac8c5b",
-        outline: "1px solid #ac8c5b",
-        bgcolor : "transparent"
-        },
-        }}
-        variant="contained"
-
-        onClick={() => {
-        dispatch(AddToCart(Product))
-
-        }}
-
-        >
-        add to cart
-        </Button>
-        )
-            
+    
             }
-
-
-
-
-
-        </Box>
-        </Box>
-            ))}
-
-            
-            
-            
-        </Box>
-        <Box
-        sx={{
-            width:"25%" ,
-            display : "flex" , 
-            flexDirection:"column",
-            alignItems : "center" , 
-            flexWrap :"wrap" ,
-        }}>
-            <Box sx={{border:"1px solid black",width : "100%" ,
+        {favProductsId.includes(Product.id) ?
+        (
+        <IconButton
+            onClick={() => {
+            dispatch(deleteFromFav(Product))
+            }}
+            sx={{ color: "gray", position: "absolute", right: "5px" }}
+        >
+            <FavoriteIcon />
+        </IconButton>   
+        ) : (
+        <IconButton
+                onClick={() => {
+            dispatch(AddToFav(Product))
+                    }}
+                    sx={{ color: "gray", position: "absolute", right: "5px" }}
+                    >
+                    <FavoriteBorderIcon />
+                    </IconButton>   
+        )
+        
+        
+        
+        }
+        
+            <Box sx={{ width: "75%", height: "65%", mx: "auto" }}  
+                onClick={() => {
+                Navigate(`/prodetails/${Product.id}`)
+                }}
+    
+            >
+                <img
+                style={{ width: "100%", height: "100%" }}
+                src={Product.imageLink}
+                alt=""
+                />
+            </Box>
+            <Box
+                className="cardcontent"
+                sx={{
+                p: "5px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                bgcolor: "#FFF",
+                mx: "auto",
+                width: "95%",
+                height: "32%",
+                borderRadius: "15px",
+                }}
+            >
+                <Typography variant="h6">{Product.Name}</Typography>
+                <Stack spacing={1}>
+                <Rating
+                    name="half-rating-read"
+                    defaultValue={Product.rate}
+                    precision={0.5}
+                    readOnly
+                />
+                </Stack>
+                <Box sx={{ mt: "5px", display: "flex" }}>
+                <del style={{ marginRight: "7px" }}> ${Product.price}</del>
+                <Typography>${Product.sale}</Typography>
+                </Box>
+    
+    
+    
+                {SelectedProductsId.includes(Product.id) ?
+    
+            (<Box className="btn-tocart" display="none">
+            <Box  sx={{ height : "27%", display : "flex"  , alignItems : "center" , justifyContent : "center"}}>
+            <Box sx={{display : "flex" , alignItems : "center" , width : "75%" , justifyContent : "center"}}>
+                <IconButton 
+                
+                onClick={() => {
+                    dispatch(decrement(Product))
+                }}
+                sx={{bgcolor : "#FFF", 
+                ":hover" : {
+                    bgcolor : "#ac8c5b"
+                    }
+                
+                }} size="small" >
+                    <RemoveIcon sx={{color : "#ac8c5b" , ":hover" : {
+                    color : "#FFF"
+                    }}} />
+                </IconButton>
+                <Typography sx={{mx:"5px" , fontWeight : "bold"}}>{ProductQuantity(Product)}</Typography>
+                <IconButton
+                onClick={() => {
+                    dispatch(increment(Product))
+                }}
+                
+                sx={{bgcolor : "#FFF" , ":hover" : {
+                    bgcolor : "#ac8c5b"
+                    }}} size="small">
+                    <AddIcon sx={{color : "#ac8c5b" , ":hover" : {
+                    color : "#FFF"
+                    }}} />
+                </IconButton>
+            </Box>
+    
+            </Box>
+            </Box>
+    
+            ) : 
+    
+    
+            (
+            <Button
+            className="btn-tocart"
+            sx={{ bgcolor: "#AC8C5B", display: "none" ,
+            ":hover": {
+            color: "#ac8c5b",
+            outline: "1px solid #ac8c5b",
+            bgcolor : "transparent"
+            },
+            }}
+            variant="contained"
+    
+            onClick={() => {
+            dispatch(AddToCart(Product))
+    
+            }}
+    
+            >
+            add to cart
+            </Button>
+            )
+                
+                }
+    
+    
+    
+    
+    
+            </Box>
+            </Box>
+                ))}
+    
+    
+    </Box>
+    
+    
+    <Box sx={{width : "17%" , display : {xs : "none" , md : "block"}}} >
+    
+    <Box sx={{border:"1px solid black",width : "100%" , p:"0 10px" ,
             height: "50px",
             borderRadius: "15px",
             my : "10px",
+
             }}>
             <Box sx={{ position: "relative" }}>
                 <Box sx={{ display: "flex", alignItems: "center" }}>
@@ -312,7 +320,9 @@ const Shop = () => {
                 </Box>
                 </Box>
             </Box>
-            <Box sx={{border:"1px solid black",width : "100%" ,
+
+
+            <Box sx={{border:"1px solid black",width : "100%" , p:"0 10px" ,
             borderRadius: "15px",
             paddingY:"10px",
             }}>
@@ -322,20 +332,50 @@ const Shop = () => {
                 </Box>
                 <Box sx={{width: "80%",margin:"10px auto", height:"1px",bgcolor:"black"}} />
                 <FormGroup sx={{my:"10px"}}>
-                    {categories.map((product)=> <Box key={product} sx={{display:"flex",alignItems:"center", 
+
+
+                <Box sx={{display:"flex",alignItems:"center", 
+                    justifyContent:"space-between", mx:"25px", color:"gray"}}>
+                        <FormControlLabel control={<Checkbox size="small" />} label="All" onClick={(e)=>SetChecked("All")} />
+                        <Typography onClick={()=>console.log(checked)}>({data.length})</Typography>
+                    </Box>
+
+
+
+
+
+                    {categories.map((cate)=> <Box key={cate} sx={{display:"flex",alignItems:"center", 
                                                                         justifyContent:"space-between", mx:"25px",
                                                                         color:"gray"}}>
-                        <FormControlLabel control={<Checkbox size="small" />} label={product} onClick={(e)=>SetChecked(product)} />
-                        <Typography onClick={()=>console.log(checked)}>(15)</Typography>
+                        <FormControlLabel control={<Checkbox size="small" />} label={cate} onClick={(e)=>{(!checked.includes(cate))?
+                                                                                            SetChecked((x) => [...x , cate]): checked.filter((x) => x !== cate)}} />
+                        <Typography onClick={()=>console.log(checked)}>({data.filter((pro) => pro.category === cate ).length})</Typography>
                     </Box>
                      )}
                 </FormGroup>
-            </Box>
-        </Box>
-    </Box> }
-
-        </Box>
+            </Box>       
+        
+    <Box sx={{width : "100%" , mt :"15px" ,border : "1px solid black" , borderRadius : "15px" , p:"10px"}}>
+    <Typography mb="10px" variant='h6'>Tags Cloud</Typography>
+    <hr/>
+    
+    {/* {Tags.map((tag) => (
+        <Button  sx={{bgcolor : "#F3F2EE" , color : "black" , m : "5px" , borderRadius : "10px" , fontSize : "12px"}} >{tag.Tag}</Button>
+    ))}
+     */}
+    
+    
     </Box>
+    
+    
+    
+    </Box>
+</>
+
+}
+
+</Box>
+        </>
     );
 }
 
