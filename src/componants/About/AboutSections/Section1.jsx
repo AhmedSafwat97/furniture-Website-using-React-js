eba91727378515b2e6f138b1349512dd228d94b3
AyaRagab555 <eng.aya.ragab@gmail.com>
2023-07-07 01:56:34 +0300

    About

---
+++ src/componants/About/AboutSections/Section1.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import {Box ,ImageList,ImageListItem,Typography} from '@mui/material';
+import BannerSection from '../../Home/HomeSections/bannerSection';
+import ScrollToTop from '../../../ExternalMethods/ScrollToTop';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import GppGoodOutlinedIcon from '@mui/icons-material/GppGoodOutlined';
+import KeyboardReturnOutlinedIcon from '@mui/icons-material/KeyboardReturnOutlined';
+import SupportAgentOutlinedIcon from '@mui/icons-material/SupportAgentOutlined';
+
+export default function Section1() {
+    const AboutDetails = [
+        {
+          id : 1,  
+          title : 'Original Products',
+          icon :<SupportAgentOutlinedIcon sx={{color: "#AC8C5B" , fontSize: '40px'}}/> ,
+        },
+        {
+          id : 2,  
+          title : 'Satisfaction Guarantee',
+          icon : <GppGoodOutlinedIcon sx={{color: "#AC8C5B" , fontSize: '40px'}} />,
+        },
+        {
+          id : 3,  
+          title : 'New Arrival Everyday',
+          icon : <KeyboardReturnOutlinedIcon sx={{color: "#AC8C5B" , fontSize: '40px'}} />,
+        },
+        {
+          id : 4,  
+          title : 'Fast & Free Shipping',
+          icon : <LocalShippingOutlinedIcon sx={{color: "#AC8C5B" , fontSize: '40px'}} /> ,
+        },
+      ];
+
+      const AboutShipping = [
+        {
+          id : 1,  
+          title : 'Products return Policy',
+          icon : <LocalShippingOutlinedIcon sx={{color: "#AC8C5B" , fontSize: '30px'}} />,
+        },
+        {
+          id : 2,  
+          title : '100% Qualityful Furniture',
+          icon : <GppGoodOutlinedIcon sx={{color: "#AC8C5B" , fontSize: '30px'}} />,
+        },
+      ];
+    return (<>
+        <ScrollToTop/>
+        <BannerSection PageName="About Us"/>
+        <Box
+        sx={{
+            minWidth: "75%"  ,
+            maxWidth:"75%",
+            mx:"auto",
+        bgcolor: "#FFF",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        my:"50px"}}>
+            <Box sx={{width : '100%',display : "flex" , justifyContent : {xs:"center",md:"space-between"},flexWrap:{xs:"wrap",md:"nowrap"}, m: "30px 0"}}>
+                {AboutDetails.map((detail)=>(
+                <Box key={detail.id} >
+                    <Box sx={{width: "70px", height : "70px",borderRadius: "50%",bgcolor : "#F3F2EE", display:"flex",
+                        alignItems: "center",justifyContent: "center",margin:'15px 0'}}>
+                        {detail.icon}
+                    </Box>
+                    <Typography sx={{margin:'10px 0'}} fontSize={"22px"} fontWeight={'600'} >{detail.title}</Typography>
+                    <Typography fontSize={"12px"} color="gray">There are many variations of passages of our </Typography>
+                </Box>
+
+                ))}
+
+            </Box>
+
+            <Box sx={{width : '100%',display : "flex" , justifyContent : {xs:"center",md:"space-between"},flexWrap:{xs:"wrap",md:"nowrap"}, m: "30px 0"}}>
+                <Box sx={{width: {sx: "100%", md:"47%"}}}>
+                <ImageList
+                  sx={{ width: "100%", height: "auto", mx: "auto" }}
+                  gap={40}
+                  variant="quilted"
+                  cols={2}
+                  rowHeight={50}
+                                   >
+                <ImageListItem cols={1}  rows={4}
+                  sx={{borderRadius: "15px",
+                    backgroundImage: 'url("../../../Imgs/photo4.jpg")',
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundOrigin: "content-box",
+                    position : 'relative',
+                    textAlign: 'center',}}
+                ></ImageListItem> 
+                <ImageListItem cols={1}  rows={1}
+                sx={{borderRadius: "15px",
+                  position : 'relative',
+                  textAlign: 'center',}}
+              ></ImageListItem>
+                 <ImageListItem cols={1}  rows={5}
+                  sx={{borderRadius: "15px",
+                    backgroundImage: 'url("../../../Imgs/photo4.jpg")',
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundOrigin: "content-box",
+                    position : 'relative',
+                    textAlign: 'center',}}
+                ></ImageListItem>
+                 <ImageListItem cols={1}  rows={3}
+                  sx={{borderRadius: "15px",
+                    backgroundImage: 'url("../../../Imgs/photo4.jpg")',
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundOrigin: "content-box",
+                    position : 'relative',
+                    textAlign: 'center',}}
+                ></ImageListItem>
+                 <ImageListItem cols={1}  rows={1}
+                  sx={{borderRadius: "15px",
+                    backgroundImage: 'url("../../../Imgs/photo4.jpg")',
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundOrigin: "content-box",
+                    position : 'relative',
+                    textAlign: 'center',}}
+                ></ImageListItem>
+                </ImageList>
+
+                </Box>
+                <Box sx={{width: {sx: "100%", md:"47%"}, py:"10px"}}>
+                  <Typography sx={{color: "gray"}}>our About us</Typography>
+                  <Typography sx={{fontWeight: '900',m : '30px 50px 30px 0' , fontSize : {xs : "20px" , md : "35px"}}} variant='h4'>We Believe in pure and organic quality </Typography>
+                  <Typography sx={{color: "gray" ,fontSize:"12px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. molestias, officia vel vero asperiores optio pariatur porro? Eligendi, beatae a. Distinctio rem architecto, error mollitia quis vero non beatae harum. Possimus corrupti, reiciendis fuga doloribus obcaecati magnam ipsa modi nemo quis, et cum sed ipsam dolorum! </Typography>
+
+                  <Box sx={{width : '100%', m: "30px 0"}}>
+                    {AboutShipping.map((Ship)=><Box key={Ship.id} sx={{display : "flex" , m: "20px 0" }}>
+                      <Box sx={{width: "50px", height : "50px",borderRadius: "50%",bgcolor : "#F3F2EE", display:"flex",
+                           alignItems: "center",justifyContent: "center",margin:'15px 15px 0 0'}}>
+                             {Ship.icon}
+                       </Box>
+                       <Box>
+                        <Typography sx={{margin:'15px 0 10px 0'}} fontSize={"22px"} fontWeight={'600'} >{Ship.title}</Typography>
+                        <Typography sx={{width:"70%"}} fontSize={"12px"} color="gray">Sure there isn't anything embarrassing hidden in the middle of text </Typography>
+                       </Box>
+                    </Box>
+                    )}
+                   </Box>
+                </Box>
+
+            </Box>
+          </Box>
+
+    </>
+    );
+}
