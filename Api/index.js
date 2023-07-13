c99adfad04c4e2e1a2486b32d3fde3b857d1bf26
AhmedSafwat97 <xppahmad@gmail.com>
2023-07-14 02:49:19 +0300

    ...

--- Api/index.js
+++ Api/index.js
@@ -4,12 +4,23 @@ const cors = require('cors');
 const products = require("./products");
 const blogs = require("./blogs");
 const ClientReview = require("./ClientReview");
-const users = require('./users');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+require('crypto').randomBytes(35).toString("hex")
+
+
+const users = require('./Users');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+ 
+// Secret key used to sign and verify the JWT
+const secretKey = 'your-secret-key';
 
+
+// // Array to store user data
+// let users = [];
 
 app.get("/", (req, res) => {
   res.send("Welcome our to online shop API...");
@@ -47,91 +58,154 @@ app.get("/Review", (req, res) => {
 
 // // For Signin and SignUp
 
-app.post('/signup', (req, res) => {
-  const { username, password } = req.body;
+// app.post('/signup', (req, res) => {
+//   const { UserName , Password , firstName , LastName , Email , Phone , Country , Town , address  } = req.body;
+
+//   // Check if the username is already taken
+//   const existingUser = users.find(user => user.UserName === UserName );
+//   if (existingUser) {
+//     return res.status(409).json({ message: 'Username already taken' });
+//   }
+
+//   // Create a new user object
+//   const newUser = { UserName , Password , firstName , LastName , Email , Phone , Country , Town , address};
+//   users.push(newUser);
+
+
+  
+//   return res.status(201).json({ message: 'User created successfully' , user: newUser });
+// });
+
+
+
+// function generateSecretKey() {
+//   // Generate a secure secret key
+//   const secretKey = crypto.randomBytes(35).toString('hex');
+//    return secretKey;
+//  }
+
+
+
+
+app.post('/signup', async (req, res) => {
+  const { UserName, Password, firstName, LastName, Email, Phone, Country, Town, address } = req.body;
 
   // Check if the username is already taken
-  const existingUser = users.find(user => user.username === username);
+  const existingUser = users.find(user => user.UserName === UserName);
   if (existingUser) {
     return res.status(409).json({ message: 'Username already taken' });
   }
+  
+if (Password.length < 6) {
+  return res.status(400).json({ message: "Password less than 6 characters" })
+}
 
   // Create a new user object
-  const newUser = { username, password };
-  users.push(newUser);
+  const newUser = { UserName, Password, firstName, LastName, Email, Phone, Country, Town, address };
+   users.push(newUser);
 
-  return res.status(201).json({ message: 'User created successfully' });
+  // Save the updated array back to the file
+  fs.writeFile('./Users.js', `module.exports = ${JSON.stringify(users)}`, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    return res.status(201).json({ message: 'User created successfully', user: newUser });
+  });
 });
 
+
+
+
+
+
 // Sign-in route
-app.post('/signin', (req, res) => {
-  const { username, password } = req.body;
+app.post('/signin', async (req, res) => {
+  const { UserName , Password } = req.body;
 
   // Find the user in the users array
-  const user = users.find(user => user.username === username && user.password === password);
+  const user = users.find(user => user.UserName === UserName && user.Password === Password);
   if (!user) {
     return res.status(401).json({ message: 'Invalid username or password' });
   }
 
-  return res.status(200).json({ message: 'Sign in successful' });
+  const userPayload = {
+    UserName: user.UserName,
+    Password: user.Password,
+    firstName: user.firstName,
+    LastName: user.LastName,
+    Email: user.Email,
+    Phone: user.Phone,
+    Country: user.Country,
+    Town: user.Town,
+    address: user.address,
+  };
+
+    // Generate a new secret key for each sign-in
+    // const secretKey = generateSecretKey();
+
+  const token = jwt.sign(userPayload, secretKey); // Generate a new token with an expiration time
+
+  // localStorage.setItem('token', token); // Save the token in session storage
+
+  return res.status(200).json({ message: 'Sign in successful' , user , token});
+
 });
+
 
 app.get('/users', (req, res) => {
   res.status(200).json(users);
 });
 
-// // Sign-up route
-// app.post('/signup', async (req, res) => {
-//   const { username, password } = req.body;
 
-//   try {
-//     // Check if the username is already taken
-//     const existingUser = await User.findOne({ where: { username } });
-//     if (existingUser) {
-//       return res.status(409).json({ message: 'Username already taken' });
-//     }
 
-//     // Create a new user
-//     const newUser = await User.create({ username, password });
 
-//     return res.status(201).json({ message: 'User created successfully' });
-//   } catch (error) {
-//     console.error('Sign-up error:', error);
-//     return res.status(500).json({ message: 'An error occurred during sign-up' });
-//   }
-// });
 
-// // Sign-in route
-// app.post('/signin', async (req, res) => {
-//   const { username, password } = req.body;
 
-//   try {
-//     // Find the user in the database
-//     const user = await User.findOne({ where: { username } });
+app.post('/blogs/comments', (req, res) => {
+  const { blogId, commentDetails } = req.body;
 
-//     // Check if the user exists and the password matches
-//     if (!user || user.password !== password) {
-//       return res.status(401).json({ message: 'Invalid username or password' });
-//     }
+  // Find the blog in the array
+  const blog = blogs.find(p => p.id === blogId);
 
-//     return res.status(200).json({ message: 'Sign in successful' });
-//   } catch (error) {
-//     console.error('Sign-in error:', error);
-//     return res.status(500).json({ message: 'An error occurred during sign-in' });
-//   }
-// });
+  if (!blog) {
+    return res.status(404).send('Blog not found');
+  }
 
-// app.get('/users', async (req, res) => {
-//   try {
-//     const users = await User.findAll();
-//     res.json(users);
-//   } catch (error) {
-//     console.error('Error retrieving users:', error);
-//     res.status(500).json({ message: 'An error occurred while retrieving users' });
-//   }
-// });
+  // Add the comment to the blog's comments array
+  blog.comments.push(commentDetails);
+
+  // Save the updated array back to the file
+  fs.writeFile('./blogs.js', `module.exports = ${JSON.stringify(blogs)}`, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return res.status(500).send('Internal server error');
+    }
+
+    // Return the updated blog object
+    res.json(blog);
+  });
+});
+
+
+
+
 
 
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`http://localhost:${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
