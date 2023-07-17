f608693bb1bfaad00706bd27a138791a1e8d2852
AyaRagab555 <eng.aya.ragab@gmail.com>
2023-07-17 10:04:35 +0300

    lkk

--- Api/Users.js
+++ Api/Users.js
@@ -1,1 +1,14 @@
-module.exports = [{"id":1,"UserName":"ahmed66","password":"$2a$10$GVeM6wLgsGUrVpaoVyaeP.4LdLdjdPGSS.bYG6s8701KBgU1vmC02","firstName":"Ahmed","LastName":"safwat","Email":"xppahmad@gmail.com","Phone":"","Country":"Egypt","Town":"","address":""}]
+module.exports = [
+  {
+    id: 1,
+    UserName: "ahmed66",
+    password: "$2a$10$GVeM6wLgsGUrVpaoVyaeP.4LdLdjdPGSS.bYG6s8701KBgU1vmC02",
+    firstName: "Ahmed",
+    LastName: "safwat",
+    Email: "xppahmad@gmail.com",
+    Phone: "",
+    Country: "Egypt",
+    Town: "",
+    address: "",
+  },
+];
